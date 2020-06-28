cbc49b324d839347f01a6fc9f398740fc902f9ea
Rob Nice <rob.w.nice@gmail.com>
2020-06-28 09:09:57 -0500

    add test dependency

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule JaTranslations.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:ex_machina, "~> 2.4", only: :test}
     ]
   end
 
