dfd98d761566649cf5a1ae6d8da687e9dd115c6d
Rob Nice <rob.w.nice@gmail.com>
2020-06-28 10:08:39 -0500

    update nav, remove phoenix css

--- assets/js/gameTranslations/GameTranslations.vue
+++ assets/js/gameTranslations/GameTranslations.vue
@@ -2,7 +2,7 @@
 <div class="flex h-screen bg-gray-200">
         <div :class="sidebarOpen ? 'block' : 'hidden'" :click="sidebarOpen = false" class="fixed z-20 inset-0 bg-black opacity-50 transition-opacity lg:hidden hidden"></div>
 
-        <div :class="sidebarOpen ? 'translate-x-0 ease-out' : '-translate-x-full ease-in'" class="fixed z-30 inset-y-0 left-0 w-1/6 transition duration-300 transform bg-gray-900 overflow-y-auto lg:translate-x-0 lg:static lg:inset-0 -translate-x-full ease-in">
+        <div :class="sidebarOpen ? 'translate-x-0 ease-out' : '-translate-x-full ease-in'" class="fixed z-30 inset-y-0 left-0 w-1/6 transition duration-300 transform bg-gray-800 overflow-y-auto lg:translate-x-0 lg:static lg:inset-0 -translate-x-full ease-in">
             <div class="flex items-center justify-center mt-8">
                 <div class="flex items-center">
                     <h1 class="text-white text-3xl mx-2">{{title}}</h1>
@@ -10,7 +10,7 @@
             </div>
 
             <nav class="mt-10">
-                <div class="flex items-center mt-4 py-2 px-6 text-gray-100 border-gray-100">
+                <div class="flex items-center mt-4 py-2 px-6 text-gray-200 border-gray-100">
                     <div class="w-full">
                         <label for="chapter-select">Chapter: </label>
                         <select id="chapter-select" v-model="chapter" class="text-black block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
@@ -20,7 +20,7 @@
                     </div>
                 </div>
 
-                <div class="flex items-center mt-4 py-2 px-6 block border-l-4 border-gray-900 text-gray-500 hover:bg-gray-600 hover:bg-opacity-25 hover:text-gray-100">
+                <div class="flex items-center mt-4 py-2 px-6 block text-gray-200 hover:bg-gray-600 hover:bg-opacity-25 hover:text-gray-100">
                     <div class="w-full">
                         <label for="scene-select">Scene: </label>
                         <select id="scene-select" v-model="scene" class="text-black block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
