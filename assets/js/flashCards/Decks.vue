dd365c86b44890d9cd48046f411b062c998feb36
Rob Nice <rob.w.nice@gmail.com>
2020-07-23 18:19:57 -0500

    Move deck card into separate template, add styling

--- assets/js/flashCards/Decks.vue
+++ assets/js/flashCards/Decks.vue
@@ -1,40 +1,55 @@
 <template>
   <div>
     <h1>Decks</h1>
-    <div class="flex container p-6">
-      <div class="py-6 w-25 min-w-full">
-        <div class="flex max-w-md bg-white shadow-lg rounded-lg overflow-hidden">
-          <div class="w-1/3 bg-cover"
-            style="background-image: url('https://images.unsplash.com/photo-1494726161322-5360d4d0eeae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80')">
-          </div>
-          <div class="w-2/3 p-4">
-            <h1 class="text-gray-900 font-bold text-2xl">Deck Title</h1>
-            <p class="mt-2 text-gray-600 text-sm">Description of the deck</p>
-            <div class="flex flex-wrap item-center mt-2">
-              <p class="w-100">Cards in this deck: 12</p>
-              <p class="w-100">Due for review: 12</p>
-            </div>
-            <div class="flex justify-end mt-3">
-              <div class="m-2">
-                <svg class="stroke-current text-blue-600 h-6 w-6" viewBox="0 0 24 24" fill="none" stroke-width="2"><path d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>
-              </div>
-              <div class="m-2">
-                <svg class="stroke-current text-red-600 h-6 w-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24"><path d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div class="flex flex-wrap justify-between p-6">
+      <DeckCard
+      v-for="dc in decks"
+      :key="'deck-card-' + dc.id"
+      :id="dc.id"
+      :title="dc.title"
+      :description="dc.description"
+      :numberOfCards="dc.numberOfCards"
+      ></DeckCard>
     </div>
   </div>
 </template>
 
 <script>
+ import DeckCard from './decks/DeckCard'
+
   export default {
     data: function () {
       return {
-
+        decks: [
+          {
+            id: 1,
+            title: "JLPT N5 Kanji",
+            description: "My deck for studying kanji for the JLPT N5",
+            numberOfCards: 500
+          },
+          {
+            id: 2,
+            title: "JLPT N5 Kanji",
+            description: "My deck for studying kanji for the JLPT N5",
+            numberOfCards: 500
+          },
+          {
+            id: 3,
+            title: "JLPT N5 Kanji",
+            description: "My deck for studying kanji for the JLPT N5",
+            numberOfCards: 500
+          },
+          {
+            id: 4,
+            title: "JLPT N5 Kanji",
+            description: "My deck for studying kanji for the JLPT N5",
+            numberOfCards: 500
+          }
+        ]
       }
+    },
+    components: {
+      DeckCard
     }
   }
 </script>
